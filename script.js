a8383b476a90ecf22738536e36ae7e9b2652027a
Ngocthai2703web1s <159770013+Ngocthai2703web1s@users.noreply.github.com>
2024-02-22 10:58:30 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ const imageList = [
   { src: "https://via.placeholder.com/500", alt: "Image 3", name: "Image Three", link: "https://example.com/image3" },
   { src: "https://via.placeholder.com/600", alt: "Image 4", name: "Image Four", link: "https://example.com/image4" },
   { src: "https://via.placeholder.com/700", alt: "Image 5", name: "Image Five", link: "https://example.com/image5" },
+  { src: "https://via.placeholder.com/800", alt: "Image 6", name: "Image Six", link: "https://example.com/image6" },
+  { src: "https://via.placeholder.com/300", alt: "Image 1", name: "Image One", link: "https://example.com/image1" },
+  { src: "https://via.placeholder.com/400", alt: "Image 2", name: "Image Two", link: "https://example.com/image2" },
+  { src: "https://via.placeholder.com/500", alt: "Image 3", name: "Image Three", link: "https://example.com/image3" },
+  { src: "https://via.placeholder.com/600", alt: "Image 4", name: "Image Four", link: "https://example.com/image4" },
+  { src: "https://via.placeholder.com/700", alt: "Image 5", name: "Image Five", link: "https://example.com/image5" },
   { src: "https://via.placeholder.com/800", alt: "Image 6", name: "Image Six", link: "https://example.com/image6" }
 ];
 
